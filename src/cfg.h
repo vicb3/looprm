0ace5efb452d2e44d088c0033ce545e3a50cf349
Vic B <vic@4ever.vip>
2025-08-27 12:20:38 +0200

    improve help string

--- src/cfg.h
+++ src/cfg.h
@@ -46,9 +46,10 @@
   "Usage:\n"\
   "    " CFG_NAME " [OPTION]... DIR SPACE\n\n"\
   "    DIR     target directory\n"\
-  "    SPACE   requested minimal free disk space (or maximal total file size\n"\
-  "            with -t) in bytes (supports suffixes K/M/G/T/P/E for\n"\
-  "            KiB/MiB/GiB/TiB/PiB/EiB)\n\n"\
+  "    SPACE   requested minimal free disk space "\
+  	"(or maximal total size with -t)\n"\
+  "            in bytes (supports suffixes K/M/G/T/P/E for "\
+  	"KiB/MiB/GiB/TiB/PiB/EiB)\n\n"\
   "Options:\n"\
   "    -a A    minimal age of empty files for -z in seconds (default: "\
   	STR(CFG_EAGE_DFL) ")\n"\
@@ -69,7 +70,8 @@
   "    -q      disable logging to stderr\n"\
   "    -r R    remove only files with names matching extended regex R\n"\
   "    -s F    log to syslog with facility F (implies -q)\n"\
-  "    -t      remove files until their total size drops below SPACE\n"\
+  "    -t      remove files until their total size is equal to or less than "\
+  	"SPACE\n"\
   "    -u      use actual disk usage (size of occupied blocks) as file size\n"\
   "    -v      increase log verbosity (use twice for debug output)\n"\
   "    -z      remove also all empty files older than A seconds (see -a)\n"\
